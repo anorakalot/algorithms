a294151d1c3fa1b5aac7b7a13362f4e1c7dc4fa8
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-12-17 14:04:41 -0800

    Add files via upload

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -3,17 +3,33 @@
 #include <ctime>
 #include <cstdlib>
 
-int main(){
-  srand(time(0));
 
-  int vector_size;
-  vector_size = 20;
-  std::vector <int> vect(vector_size);
-  int temp;
+void fill_vect (std::vector<int> &vect){
+  int vect_size;
+  int min;
+  int max;
+  std::cout << "What size vector do you want?" <<std:: endl;
+  std::cin >> vect_size;
+  std::cout << "What is the min?" << std::endl;
+  std::cin >> min;
+  std::cout << "what is the max!" << std::endl;
+  std::cin >> max;
+  vect.resize(vect_size);
 
   for (int x = 0; x < vect.size(); ++x){
-    vect.at(x) = 10 + (rand()% (10-0+1));
+      vect.at(x) = min+(rand()%(max-min+1));
   }
+}
+
+
+
+
+
+int main(){
+  srand(time(0));
+  std::vector<int> vect;
+  int temp;
+  fill_vect(vect);
 
   std::cout << "unsorted list " << std::endl;
   for (int x = 0; x < vect.size(); ++x){
@@ -35,5 +51,6 @@ int main(){
   for (int x = 0; x < vect.size(); ++x){
     std::cout << vect.at(x) << " ";
   }
+  std::cout << std::endl;
 
 }
